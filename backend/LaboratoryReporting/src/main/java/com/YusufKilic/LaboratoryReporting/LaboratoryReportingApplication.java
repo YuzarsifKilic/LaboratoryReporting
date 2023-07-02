b30a6ca36eaf225d10230ee5d33d72e3cc7dffe0
Yusuf Kılıç <yuzarsifkilic@gmail.com>
2023-07-02 22:15:52 +0300

    initial commit

---
+++ backend/LaboratoryReporting/src/main/java/com/YusufKilic/LaboratoryReporting/LaboratoryReportingApplication.java
@@ -0,0 +1,13 @@
+package com.YusufKilic.LaboratoryReporting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboratoryReportingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaboratoryReportingApplication.class, args);
+	}
+
+}
