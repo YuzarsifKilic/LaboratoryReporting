cd4686974784e670ed7d62203f3d90b19f61415e
Yusuf Kılıç <92811030+YuzarsifKilic@users.noreply.github.com>
2023-07-31 16:13:16 +0300

    Update StartupConfig.java
    changed startup configurations

--- laboratory_reporting/api/LaboratoryReporting/src/main/java/com/YusufKilic/LaboratoryReporting/config/StartupConfig.java
+++ laboratory_reporting/api/LaboratoryReporting/src/main/java/com/YusufKilic/LaboratoryReporting/config/StartupConfig.java
@@ -16,11 +16,16 @@ public class StartupConfig implements CommandLineRunner {
     private final UserService userService;
     private final LaborantRepository laborantRepository;
     private final PatientRepository patientRepository;
+    private final ReportService reportService;
 
-    public StartupConfig(UserService userService, LaborantRepository laborantRepository, PatientRepository patientRepository) {
+    public StartupConfig(UserService userService,
+                         LaborantRepository laborantRepository,
+                         PatientRepository patientRepository,
+                         ReportService reportService) {
         this.userService = userService;
         this.laborantRepository = laborantRepository;
         this.patientRepository = patientRepository;
+        this.reportService = reportService;
     }
 
     @Override
@@ -43,6 +48,19 @@ public class StartupConfig implements CommandLineRunner {
                 .build();
         laborantRepository.save(laborant2);
 
+        Laborant laborant3 = Laborant.builder()
+                .id(3L)
+                .firstName("Ahmet")
+                .lastName("Kılıç")
+                .build();
+        laborantRepository.save(laborant3);
+
+        Laborant laborant4 = Laborant.builder()
+                .id(4L)
+                .firstName("Yusuf")
+                .lastName("Budak")
+                .build();
+        laborantRepository.save(laborant4);
         Patient patient = Patient.builder()
                 .id(1L)
                 .firstName("Zeynep")
@@ -58,5 +76,48 @@ public class StartupConfig implements CommandLineRunner {
                 .identificationNumber(1234567890)
                 .build();
         patientRepository.save(patient2);
+
+        Patient patient3 = Patient.builder()
+                .id(3L)
+                .firstName("Zeynep")
+                .lastName("Taşçı")
+                .identificationNumber(1234527890)
+                .build();
+        patientRepository.save(patient3);
+
+        Patient patient4 = Patient.builder()
+                .id(4L)
+                .firstName("Elif")
+                .lastName("Kılıç")
+                .identificationNumber(1234521890)
+                .build();
+        patientRepository.save(patient4);
+
+        createReport(1L, 1L);
+        createReport(1L, 2L);
+        createReport(1L, 3L);
+        createReport(1L, 4L);
+        createReport(2L, 1L);
+        createReport(2L, 2L);
+        createReport(2L, 3L);
+        createReport(2L, 4L);
+        createReport(3L, 1L);
+        createReport(3L, 2L);
+        createReport(3L, 3L);
+        createReport(3L, 4L);
+        createReport(4L, 1L);
+        createReport(4L, 2L);
+        createReport(4L, 3L);
+        createReport(4L, 4L);
+    }
+
+    private void createReport(long patientId, long laborantId) {
+        for (int i = 0; i < 200; i++) {
+            reportService.createReport(new CreateReportRequest(
+                    "Sinüzit",
+                    "Soğuk Algınlığı",
+                    patientId,
+                    laborantId));
+        }
     }
 }
