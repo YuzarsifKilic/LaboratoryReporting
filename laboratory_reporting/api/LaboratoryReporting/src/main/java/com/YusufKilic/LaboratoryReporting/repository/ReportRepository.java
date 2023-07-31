17f822aa5efcde6f565f4abfce22707745ac10cc
Yusuf Kılıç <92811030+YuzarsifKilic@users.noreply.github.com>
2023-07-31 16:11:31 +0300

    Update ReportRepository.java
    solved slowness problem using jpql

--- laboratory_reporting/api/LaboratoryReporting/src/main/java/com/YusufKilic/LaboratoryReporting/repository/ReportRepository.java
+++ laboratory_reporting/api/LaboratoryReporting/src/main/java/com/YusufKilic/LaboratoryReporting/repository/ReportRepository.java
@@ -4,4 +4,12 @@ import com.YusufKilic.LaboratoryReporting.model.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReportRepository extends JpaRepository<Report, Long> {
+
+  @Query(
+     "select r from Report r inner join Laborant l on r.laborant.id = l.id where l.firstName = :firstName and l.lastName = :lastName")
+    List<Report> findReportsByLaborant(@Param("firstName") String firstName, @Param("lastName") String lastName);
+
+    @Query(
+            "select r from Report r inner join Patient p on r.patient.id = p.id where p.firstName = :firstName and p.lastName = :lastName")
+    List<Report> findReportsByPatient(@Param("firstName") String firstName, @Param("lastName") String lastName);
 }
